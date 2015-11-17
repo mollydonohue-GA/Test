c062e0c1619eb5542200e09b6dc7a39fd2158097
Molly Donohue <mollykennan@gmail.com>
2015-11-17 09:33:19 -0500

    merge practice

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("makin merge problems");
+
+var array = ['dog', 'cat', 'hampster', 'dragon'];
+
+console.log(array[0])
+
+// if (i = 0; i < array.length; i++) {
+// 	console.log(array[i]);
+// }
