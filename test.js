4c25be17f148dc31df7c3b5aad7af5241b0587fa
Molly Donohue <mollykennan@gmail.com>
2015-11-17 09:52:48 -0500

    trying to make merge conflicts for practice

--- test.js
+++ test.js
@@ -4,6 +4,12 @@ var array = ['dog', 'cat', 'hampster', 'dragon'];
 
 console.log(array[3]);
 
+console.log(array[2]);
+
+console.log(array[1]);
+
+console.log(array[0]);
+
 // if (i = 0; i < array.length; i++) {
 // 	console.log(array[i]);
 // }
