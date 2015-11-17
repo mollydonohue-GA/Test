ff1d86a71c3a228086e0e999a51bb441f41de546
Molly Donohue <mollykennan@gmail.com>
2015-11-17 09:35:25 -0500

    makin merge conflicts

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-console.log("makin merge problems");
+console.log("makin merge problems!!!!!!");
 
 var array = ['dog', 'cat', 'hampster', 'dragon'];
 
-console.log(array[0])
+console.log(array[3]);
 
 // if (i = 0; i < array.length; i++) {
 // 	console.log(array[i]);
